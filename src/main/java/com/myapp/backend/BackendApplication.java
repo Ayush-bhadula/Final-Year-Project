23988647919ff06021a6f9297689f97665bb3f35
Rishav00123 <147631345+Rishav00123@users.noreply.github.com>
2026-03-27 07:35:36 +0530

    yes

--- src/main/java/com/myapp/backend/BackendApplication.java
+++ src/main/java/com/myapp/backend/BackendApplication.java
@@ -2,15 +2,10 @@ package com.myapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.myapp.backend.repository")
-@EntityScan(basePackages = "com.myapp.backend.model")
 public class BackendApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
 }
