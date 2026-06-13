7598382cf47042d5210cac9c16e68b3e82f79137
Ayush-bhadula <ayushbhadula57@gmail.com>
2026-06-13 23:14:28 +0530

    mkc is project ki

--- src/main/resources/static/Signup.js
+++ src/main/resources/static/Signup.js
@@ -47,7 +47,7 @@ document.getElementById("signupBtn").addEventListener("click", async () => {
   };
 
   try {
-    const res = await fetch("https://final-year-project-production-a8ea.up.railway.app/api/auth/signup", {
+    const res = await fetch("https://final-year-project-3-pn8n.onrender.com/api/auth/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
